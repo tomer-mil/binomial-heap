fef7f16ca3ba3ecc28900c837dcf00a7fe8d95ba
tomer-mil <tomer.mildworth@gmail.com>
2023-06-19 14:16:20 +0300

    More tests added and fixed meld

--- src/BinomialHeapTest.java
+++ src/BinomialHeapTest.java
@@ -31,54 +31,69 @@ class BinomialHeapTest {
         return keys;
     }
 
+    public int countTreesFromList(List<BinomialHeap.HeapItem> itemList) {
+        return Integer.toBinaryString(itemList.size()).replace("0", "").length();
+    }
+
+    public void clearHeapUsingDelete(BinomialHeap heap, List<BinomialHeap.HeapItem> itemList) {
+        for (BinomialHeap.HeapItem item : itemList) {
+            heap.delete(item);
+        }
+    }
+
+    public void clearHeapUsingDeleteMin(BinomialHeap heap, List<BinomialHeap.HeapItem> itemList) {
+        for (BinomialHeap.HeapItem item : itemList) {
+            heap.deleteMin();
+        }
+    }
+
     @BeforeEach
     void setUp() {
         int numOfKeys = 100;
 
         heap = new BinomialHeap();
-//
-//        emptyHeap = new BinomialHeap();
-//        oneItemHeap = new BinomialHeap();
-//
-//        multipleItemsHeap1 = new BinomialHeap();
-//        multipleItemsHeap2 = new BinomialHeap();
-//
-//        testedItemsList1 = new ArrayList<>();
-//        testedItemsList2 = new ArrayList<>();
-//
-//
-//        int[] keys1 = this.generateKeys(numOfKeys);
-//        int[] keys2 = this.generateKeys(numOfKeys);
-//
-//        System.out.println("keys1: " + Arrays.toString(keys1));
-//        System.out.println("keys2: " + Arrays.toString(keys2));
-//
-//        for (int i = 0; i < numOfKeys; i++) {
-//
-//            BinomialHeap.HeapItem item1 = multipleItemsHeap1.insert(keys1[i], "");
-//            BinomialHeap.HeapItem item2 = multipleItemsHeap2.insert(keys2[i], "");
-//
-//            testedItemsList1.add(item1);
-//            testedItemsList2.add(item2);
-//
-//            if (i == 0) { this.oneItemHeapItem = oneItemHeap.insert(testedItemsList1.get(0).key, ""); }
-//        }
-//
-//        testedItemsList1.sort(Comparator.comparingInt(a -> a.key));
-//        testedItemsList2.sort(Comparator.comparingInt(a -> a.key));
-//
-//        this.multipleItemsHeap1MinItem = testedItemsList1.get(0);
-//        this.multipleItemsHeap2MinItem = testedItemsList2.get(0);
-//
-//        Arrays.sort(keys1);
-//        Arrays.sort(keys2);
+
+        emptyHeap = new BinomialHeap();
+        oneItemHeap = new BinomialHeap();
+
+        multipleItemsHeap1 = new BinomialHeap();
+        multipleItemsHeap2 = new BinomialHeap();
+
+        testedItemsList1 = new ArrayList<>();
+        testedItemsList2 = new ArrayList<>();
+
+
+        int[] keys1 = this.generateKeys(numOfKeys);
+        int[] keys2 = this.generateKeys(numOfKeys);
+
+        System.out.println("keys1: " + Arrays.toString(keys1));
+        System.out.println("keys2: " + Arrays.toString(keys2));
+
+        for (int i = 0; i < numOfKeys; i++) {
+
+            BinomialHeap.HeapItem item1 = multipleItemsHeap1.insert(keys1[i], "");
+            BinomialHeap.HeapItem item2 = multipleItemsHeap2.insert(keys2[i], "");
+
+            testedItemsList1.add(item1);
+            testedItemsList2.add(item2);
+
+            if (i == 0) { this.oneItemHeapItem = oneItemHeap.insert(testedItemsList1.get(0).key, ""); }
+        }
+
+        testedItemsList1.sort(Comparator.comparingInt(a -> a.key));
+        testedItemsList2.sort(Comparator.comparingInt(a -> a.key));
+
+        this.multipleItemsHeap1MinItem = testedItemsList1.get(0);
+        this.multipleItemsHeap2MinItem = testedItemsList2.get(0);
+
+        Arrays.sort(keys1);
+        Arrays.sort(keys2);
 
     }
 
     @Test
     void testInsert() {
         // Test basic insertion
-
         List<Integer> testList = Arrays.asList(349, 278, 169, 419, 44, 37, 170, 296, 457, 120, 196, 281, 118, 74, 326, 159, 122, 307, 37, 440, 94, 371, 367, 126, 50, 44, 379, 48, 129, 480, 101, 272, 302, 177, 307, 65, 91, 350, 465, 309, 80, 239, 134, 451, 456, 258, 368, 300, 91, 406, 394, 387, 235, 197, 82, 267, 134, 473, 365, 69, 434, 475, 474, 168, 132, 212, 203, 18, 495, 254, 24, 484, 149, 3, 143, 193, 88, 409, 10, 264, 247, 142, 315, 456, 320, 123, 217, 61, 418, 409, 487, 377, 20, 315, 262, 181, 352, 395, 90, 271);
 
         for (int i = 0; i < testList.size(); i++) {
@@ -87,7 +102,7 @@ class BinomialHeapTest {
 
         BinomialHeap.HeapItem item = heap.insert(1, "item1");
         assertNotNull(item);
-        assertEquals(1, heap.size());
+        assertEquals(testList.size() + 1, heap.size());
         assertEquals(item, heap.findMin());
 
 //        heap.print();
@@ -95,7 +110,7 @@ class BinomialHeapTest {
         // Test insertion that changes min
         BinomialHeap.HeapItem item2 = heap.insert(0, "item0");
         assertNotNull(item2);
-        assertEquals(2, heap.size());
+        assertEquals(2 + testList.size(), heap.size());
         assertEquals(item2, heap.findMin());
     }
 
@@ -115,6 +130,12 @@ class BinomialHeapTest {
         heap.insert(3, "item3");
         heap.deleteMin();
         assertEquals(2, heap.size());
+
+        clearHeapUsingDeleteMin(multipleItemsHeap1, testedItemsList1);
+        assertTrue(multipleItemsHeap1.empty());
+
+        clearHeapUsingDeleteMin(multipleItemsHeap2, testedItemsList2);
+        assertTrue(multipleItemsHeap2.empty());
     }
 
     @Test
@@ -163,6 +184,13 @@ class BinomialHeapTest {
         BinomialHeap.HeapItem item = heap.insert(1, "item1");
         heap.delete(item);
         assertEquals(0, heap.size());
+
+        this.clearHeapUsingDelete(multipleItemsHeap1, testedItemsList1);
+        assertTrue(multipleItemsHeap1.empty());
+
+        this.clearHeapUsingDelete(multipleItemsHeap2, testedItemsList2);
+        assertTrue(multipleItemsHeap2.empty());
+
     }
 
     @Test
@@ -196,6 +224,9 @@ class BinomialHeapTest {
         // Test numTrees on non-empty heap
         heap.insert(1, "item1");
         assertEquals(1, heap.numTrees());
+
+        assertEquals(this.countTreesFromList(testedItemsList1), multipleItemsHeap1.numTrees());
+        assertEquals(this.countTreesFromList(testedItemsList2), multipleItemsHeap2.numTrees());
     }
 
 }
